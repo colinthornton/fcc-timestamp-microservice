eaf48a8d7b3c086529257922b87e25f36c0506d0
Colin Thornton <colinthorntontx@gmail.com>
2017-11-25 15:35:37 +0900

    Implement input validation

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:time', (req, res) => {
-  const time = moment(req.params.time);
+  console.log(`req.params.time is ${req.params.time}`);
+  console.log('trying to parse natural language');
+  let time = moment(req.params.time, 'MMMM D, YYYY', true);
   const json = {
     unix: null,
     natural: null
   };
+  if (!time.isValid()) {
+    console.log('trying to parse unix')
+    time = moment.unix(Number(req.params.time));
+  }
   if (time.isValid()) {
-    json.unix = time.format('X')
+    console.log('it\'s a valid input!');
+    json.unix = Number(time.format('X'));
     json.natural = time.format('MMMM D, YYYY');
   }
+  console.log('sending json\n***************');
   res.json(json);
 })
 
