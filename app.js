3907be415a55e1a8a0a0729a64a0af3c65aed6ea
Colin Thornton <colinthorntontx@gmail.com>
2017-11-25 10:42:31 +0900

    Complete app.js functionality

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const moment = require('moment');
 
 app.get('/', (req, res) => {
-  res.send("hello");
+  res.send('give me some times in that url');
 });
+
+app.get('/:time', (req, res) => {
+  const time = moment(req.params.time);
+  const json = {
+    unix: null,
+    natural: null
+  };
+  if (time.isValid()) {
+    json.unix = time.format('X')
+    json.natural = time.format('MMMM D, YYYY');
+  }
+  res.json(json);
+})
 
 app.listen(8080, () => console.log('Server open on 8080'));
